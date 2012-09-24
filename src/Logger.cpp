28e21c9f9cffc808798e7c76df245446ee21fea5
Matthias Krull <m.krull@uninets.eu>
2012-09-24 18:36:34 +0200

    forgot to git add logger files. fixed

---
+++ src/Logger.cpp
@@ -0,0 +1,128 @@
+/*
+ * Logger.cpp
+ *
+ *  Created on: 21.09.2012
+ *      Author: mak
+ */
+
+#include "Logger.h"
+#include <map>
+
+using namespace std;
+
+namespace ZabbixNotifier {
+
+    Logger* Logger::MInstance = 0;
+    int Logger::level = INFO;
+
+    Logger& Logger::Instance(){
+        if (MInstance == 0){
+            MInstance = new Logger();
+        }
+
+        return *MInstance;
+    }
+
+    void Logger::init(){
+        openlog("zabbix-notifier", LOG_PID | LOG_CONS | LOG_NDELAY, LOG_USER);
+    }
+
+    bool Logger::set_level(string level){
+        map<string, int> level_map;
+
+        level_map["EMERG"]   = EMERG;
+        level_map["ALERT"]   = ALERT;
+        level_map["CRIT"]    = CRIT;
+        level_map["ERR"]     = ERR;
+        level_map["WARNING"] = WARNING;
+        level_map["NOTICE"]  = NOTICE;
+        level_map["INFO"]    = INFO;
+        level_map["DEBUG"]   = DEBUG;
+
+        Logger::level = level_map[level];
+
+        return true;
+    }
+
+    void Logger::Cleanup(){
+        closelog();
+        delete MInstance;
+        MInstance = 0;
+    }
+
+    void Logger::emerg(string line){
+        if (Logger::level >= 0)
+            syslog(LOG_EMERG, "%s", line.c_str());
+    }
+
+    void Logger::alert(string line){
+        if (Logger::level >= 1)
+            syslog(LOG_ALERT, "%s", line.c_str());
+    }
+
+    void Logger::crit(string line){
+        if (Logger::level >= 2)
+            syslog(LOG_CRIT, "%s", line.c_str());
+    }
+
+    void Logger::err(string line){
+        if (Logger::level >= 3)
+            syslog(LOG_ERR, "%s", line.c_str());
+    }
+
+    void Logger::warn(string line){
+        if (Logger::level >= 4)
+            syslog(LOG_WARNING, "%s", line.c_str());
+    }
+
+    void Logger::notice(string line){
+        if (Logger::level >= 5)
+            syslog(LOG_NOTICE, "%s", line.c_str());
+    }
+
+    void Logger::info(string line){
+        if (Logger::level >= 6)
+            syslog(LOG_INFO, "%s", line.c_str());
+    }
+
+    void Logger::debug(string line){
+        if (Logger::level >= 7)
+            syslog(LOG_DEBUG, "%s", line.c_str());
+    }
+
+    // TODO ugly..
+    void Logger::log(string line, string level){
+        // propably most used first
+        if (Logger::level >= 5 && line.compare("NOTICE") == 0){
+            notice(line);
+        }
+        else if (Logger::level >= 6 && line.compare("INFO") == 0){
+            info(line);
+        }
+        else if (Logger::level >= 7 && line.compare("DEBUG") == 0){
+            debug(line);
+        }
+        // hopefuly not used last
+        else if (Logger::level >= 4 && line.compare("WARN") == 0){
+            warn(line);
+        }
+        else if (Logger::level >= 3 && line.compare("ERR") == 0){
+            err(line);
+        }
+        else if (Logger::level >= 2 && line.compare("CRIT") == 0){
+            crit(line);
+        }
+        else if (Logger::level >= 1 && line.compare("ALERT") == 0){
+            alert(line);
+        }
+        else if (Logger::level >= 0 && line.compare("EMERG") == 0){
+            emerg(line);
+        }
+    }
+
+    Logger::Logger() {
+        init();
+
+        atexit(&Cleanup);
+    }
+}
---
+++ src/Logger.h
@@ -0,0 +1,54 @@
+/*
+ * Logger.h
+ *
+ *  Created on: 21.09.2012
+ *      Author: mak
+ */
+
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+#include <syslog.h>
+#include <string>
+#include <cstdlib>
+
+#define EMERG   0   /* system is unusable */
+#define ALERT   1   /* action must be taken immediately */
+#define CRIT    2   /* critical conditions */
+#define ERR     3   /* error conditions */
+#define WARNING 4   /* warning conditions */
+#define NOTICE  5   /* normal but significant condition */
+#define INFO    6   /* informational */
+#define DEBUG   7   /* debug-level messages */
+
+using namespace std;
+
+namespace ZabbixNotifier {
+
+    class Logger {
+        public:
+            static Logger& Instance();
+            Logger();
+
+            void emerg(string line);
+            void alert(string line);
+            void crit(string line);
+            void err(string line);
+            void warn(string line);
+            void notice(string line);
+            void info(string line);
+            void debug(string line);
+            void log(string line, string level);
+
+            bool set_level(string level);
+        private:
+            void init();
+            static void Cleanup();
+
+            static int level;
+            static Logger* MInstance;
+    };
+
+}
+
+#endif /* LOGGER_H_ */
